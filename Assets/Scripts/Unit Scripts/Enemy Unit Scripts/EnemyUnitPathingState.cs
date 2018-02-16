345d4c65e898b0875b5d6232455fe0112f957de1
Luaithe <jwflannery93@gmail.com>
2018-02-16 18:10:17 +1000

    Units now take the shortest route to their targets.

--- Assets/Scripts/Unit Scripts/Enemy Unit Scripts/EnemyUnitPathingState.cs
+++ Assets/Scripts/Unit Scripts/Enemy Unit Scripts/EnemyUnitPathingState.cs
@@ -44,6 +44,7 @@ public class EnemyUnitPathingState : UnitPathingState {
                 if (n.cost < lowestCost)
                 {
                     lowestCostNode = n;
+                    lowestCost = n.cost;
                     possibleAttackTargets = adjacentUnits;
                 }
             }
